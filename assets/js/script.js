6f8b5d51e3f33cdc17c31fba27108f90b8a951f8
AnousoneM <anousone.mounivongs@gmail.com>
2023-07-31 01:31:32 +0200

    mise à jour du script pour éviter des erreurs dans la console

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,20 @@
-// nous effectuons le calcul dynamique de la TVA lorsque l'utilisateur rentre un montant TTC
-document.getElementById('amount').addEventListener('input', function () {
-    calculateHT(this)
-})
+// nous recupérons le champ qui contient le montant TTC
+if (document.getElementById('amount') !== null) {
+    // nous effectuons le calcul dynamique de la TVA lorsque l'utilisateur rentre un montant TTC
+    document.getElementById('amount').addEventListener('input', function () {
+        calculateHT(this)
+    })
+}
 
-// nous effectuons le calcul dynamique de la TVA lorsque l'utilisateur change le type de frais
-document.getElementById('type').addEventListener('change', function () {
-    calculateHT(document.getElementById('amount'))
-})
+// nous recupérons le select qui contient les options
+let type = document.getElementById('type')
+if (type != null) {
+    // nous effectuons le calcul dynamique de la TVA lorsque l'utilisateur change le type de frais
+    document.getElementById('type').addEventListener('change', function () {
+        calculateHT(document.getElementById('amount'))
+    })
+}
+
 
 // fonction permettant de calculer le montant HT et le montant TVA,
 // il prend en paramètre l'élément input qui contient le montant TTC
@@ -15,7 +23,6 @@ function calculateHT(element) {
     let selectType = document.getElementById('type')
     // nous recupérons la valeur du data-tva de l'option
     let tva = selectType.options[selectType.selectedIndex].dataset.tva
-
 
     if (tva) {
         // nous calculons le montant HT et nous l'insérons dans le champ HT
